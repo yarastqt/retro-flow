749496f270688e8c2284fb6dc052cac330a4d76d
yarastqt <yarastqt@gmail.com>
2025-12-07 00:24:21 +0300

    feat: impl slots for colum

--- src/pages/board/ui/column.tsx
+++ src/pages/board/ui/column.tsx
@@ -1,11 +1,31 @@
 import type { FC, ReactNode } from 'react'
 
+import { createSlot, useSlots } from '@app/shared/lib/react-slots'
+
 export interface ColumnProps {
   children: ReactNode
 }
 
-export const Column: FC<ColumnProps> = (props) => {
-  const { children } = props
+const TitleSlot = createSlot()
+const CardsSlot = createSlot()
 
-  return <div>{children}</div>
+const Column: FC<ColumnProps> = (props) => {
+  const slots = useSlots(props)
+
+  const title = slots.get(TitleSlot)
+  const cards = slots.get(CardsSlot)
+
+  return (
+    <div className="w-[336px]">
+      <div>{title?.rendered}</div>
+      <div>{cards?.rendered}</div>
+    </div>
+  )
 }
+
+const _Column = Object.assign(Column, {
+  Title: TitleSlot,
+  Cards: CardsSlot,
+})
+
+export { _Column as Column }
