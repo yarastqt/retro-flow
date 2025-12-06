9ab4294295c5d5f437cb75944fb95a7c11582daa
yarastqt <yarastqt@gmail.com>
2025-12-06 23:24:10 +0300

    chore: setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import tsconfigPath from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
     outDir: 'build',
   },
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: ['babel-plugin-react-compiler'],
