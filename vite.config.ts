afae2076c6ced1dcabf131ecf2601d9f9de47a2d
yarastqt <yarastqt@gmail.com>
2025-12-06 19:47:25 +0300

    chore: setup deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tsconfigPath from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPath()],
 })
